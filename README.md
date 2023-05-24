2138b7c998103abb8b560da78f86b3fde7802f9f
FolkusKyiv <abdulah@ukr.net>
2023-05-24 18:27:30 +0200

    README.mdE ver 2 Added comments, updated specs structure

--- README.md
+++ README.md
@@ -6,12 +6,57 @@
 -  Install [Node.js 18.16.0](https://nodejs.org/en/download)
 -  (optional) Install and set up [Docker](https://www.docker.com/) on the local machine
 
-### There two ways to run test -- locally and though the Docker container
-- To run locally, use ```npx cypress open```, to launch cypress test suite
 
+## Execution
+### There two ways to run test -- locally and though the Docker container
+- To run locally, install cypress and other dependencies (faker for email generation) 
+```
+npm install cypress
+npm install faker --save-dev
+```
+-  use ```npx cypress open```, to launch cypress test suite
+
+
+### --------------------------------------------------------------------------------
 - To run in a Docker container, after you set up Docker app, run ```docker-compose up --build``` in the terminal.
-You can stop a container by running ```docker stop $(docker ps -aq)``` 
+
+- You can stop a container by running ```docker stop $(docker ps -aq)``` 
 or check the status of active containers by ```docker ps -a```.
 To remove a container, use ```docker rm $(docker ps -aq)```
 
 For Docker runs, screenshots and videos of the test runs will be generated in /cypress/
+### --------------------------------------------------------------------------------
+
+There are 3 test spec
+1. LoginRegister.cy.js -- it looks briefly at the landing, log in and register pages
+2. RegisterFunctional.cy.js -- checks that registration is processed fine with different cases (valid/invalid email, no email and required field not selected
+   )
+3. RegisterPage.cy.js -- checks UI and CSS values of all visible components on the registration page
+### --------------------------------------------------------------------------------
+# Note 1
+For valid email test at RegisterFunctionality.cy.js, I have not proceeded to actually register users, as this is PROD and I do not want to cause commotion in your DB (but the possible checks will follow similar patterns to other checks + check that the email is no longer valid for registration)
+
+# Note 2
+
+If you have cypress already installed locally in Users\Appdata\Local\Cypress, you can run just
+```
+npm install faker --save-dev
+npx cypress open
+```
+to launch the tests
+Alternatively, you can also do ```npx cypress run -b chrome``` to launch headless execution of the tests in chrome
+
+# Note 3
+For the sake of demonstration, the checks assume that everything is OK and according to design doc. In reality, it is not always the case. 
+
+Import of css constants from design doc can be used, for examaple, by
+constants.js
+``` 
+
+export const MY_CONSTANT = 'Hello, World!';
+export const ANOTHER_CONSTANT = 'Pipedrive is awesome!';
+```
+and then just import these in your specs file 
+```
+import { MY_CONSTANT, ANOTHER_CONSTANT } from './constants';
+```
--- cypress/e2e/login-register/RegisterPage.cy.js
+++ cypress/e2e/login-register/RegisterPage.cy.js
@@ -1,126 +1,135 @@
-describe('Register Page UI', () => {
+describe('Register Page UI DOM check', () => {
 
-  beforeEach(() => {
-    cy.visit('https://www.pipedrive.com/uk')
     const viewportWidth = 1280
     const viewportHeight = 720
-    cy.viewport(viewportWidth, viewportHeight)
-    cy.get("#onetrust-accept-btn-handler").should("be.visible")
-    cy.get("#onetrust-accept-btn-handler").click()
-  })
-  it("Registration page UI and  DOM  check",
-      () => {
-        const registerBtnTxt = "Try it free";
-        const registerLink = "https://www.pipedrive.com/en/register"
-        const loginTabURL = 'https://app.pipedrive.com/auth/login'
-        const fieldHeight = "56px"
-        const fieldWidth = "420px"
-        const fontFamily = "Inter, sans-serif"
-        const fontWeight400 = '400'
-        const fontWeight700 = "700"
-        const fontLineHeight22 = "22px"
-        const listOfLanguages = ["English (US)", "Čeština", "Deutsch", "Eesti", "Español (España)",
-          "Español (América Latina)", "Français", "Bahasa Indonesia", "Italiano", "Japanese (日本語)",
-          "Korean (한국어)", "Latviešu valoda", "Nederlands", "Norsk", "Polski", "Português (BR)", "Русский",
-          "Suomi", "Svenska", "Türkçe", "Українська", "Chinese (繁體中文)"]
-        const listOfLanguagesLinks = ["/en/register", "/cs/register", "/de/register", "/et/register",
-          "/es-es/register", "/es/register", "/fr/register", "/id/register", "/it/register", "/ja/register",
-          "/ko/register", "/lv/register", "/nl/register", "/nb/register", "/pl/register", "/pt/register",
-          "/ru/register", "/fi/register", "/sv/register", "/tr/register", "/uk/register", "/zh-hant/register",]
-        const h3LineHeight = "44px"
-        const h3FontSize = "34px"
-        const fontSize12 = "12px"
-        const fontSize14 = "14px"
-        const h3TextAlign = "center"
-        const h3TextColor = "rgb(25, 36, 53)"
-        const fontcolor1 = "rgb(25, 36, 53)"
-        const fontColor2 = "rgb(101, 110, 122)"
-        const colorWhite = "rgb(255, 255, 255)"
-        const tOSandPN =["https://www.pipedrive.com/en/terms-of-service",
-          "https://www.pipedrive.com/en/privacy"]
+    const registerBtnTxt = "Try it free";
+    const registerLink = "https://www.pipedrive.com/en/register"
+    const loginTabURL = 'https://app.pipedrive.com/auth/login'
 
-        const expectedLinksFooter = [
-          {
+    const fieldHeight = "56px"
+    const fieldWidth = "420px"
+    const fontFamily = "Inter, sans-serif"
+    const fontWeight400 = '400'
+    const fontWeight700 = "700"
+    const fontLineHeight22 = "22px"
+    const h3LineHeight = "44px"
+    const h3FontSize = "34px"
+    const fontSize12 = "12px"
+    const fontSize14 = "14px"
+    const h3TextAlign = "center"
+
+    const h3TextColor = "rgb(25, 36, 53)"
+    const fontcolor1 = "rgb(25, 36, 53)"
+    const fontColor2 = "rgb(101, 110, 122)"
+    const colorWhite = "rgb(255, 255, 255)"
+
+    const expectedLinksFooter = [
+        {
             url: 'https://www.pipedrive.com/en/terms-of-service',
             text: 'Terms of Service'
-          },
-          {
+        },
+        {
             url: 'https://www.pipedrive.com/en/privacy',
             text: 'Privacy Notice'
-          },
-          {
+        },
+        {
             url: 'https://www.pipedrive.com/en/cookie-notice',
             text: 'Cookie Notice'
-          },
-          {
+        },
+        {
             url: 'https://www.pipedrive.com/en/sitemap',
             text: 'Site map'
-          }
-        ]
+        }
+    ]
 
+    //I show both ways how such case can be handled
+
+    const tOSandPN =["https://www.pipedrive.com/en/terms-of-service",
+        "https://www.pipedrive.com/en/privacy"]
+
+    const listOfLanguages = ["English (US)", "Čeština", "Deutsch", "Eesti", "Español (España)",
+        "Español (América Latina)", "Français", "Bahasa Indonesia", "Italiano", "Japanese (日本語)",
+        "Korean (한국어)", "Latviešu valoda", "Nederlands", "Norsk", "Polski", "Português (BR)", "Русский",
+        "Suomi", "Svenska", "Türkçe", "Українська", "Chinese (繁體中文)"]
+    const listOfLanguagesLinks = ["/en/register", "/cs/register", "/de/register", "/et/register",
+        "/es-es/register", "/es/register", "/fr/register", "/id/register", "/it/register", "/ja/register",
+        "/ko/register", "/lv/register", "/nl/register", "/nb/register", "/pl/register", "/pt/register",
+        "/ru/register", "/fi/register", "/sv/register", "/tr/register", "/uk/register", "/zh-hant/register",]
+
+    //Before each to land on UA landing and navigate to register page
+
+    beforeEach(() => {
+        cy.visit('https://www.pipedrive.com/uk')
+        cy.viewport(viewportWidth, viewportHeight)
+        cy.get("#onetrust-accept-btn-handler").should("be.visible")
+        cy.get("#onetrust-accept-btn-handler").click()
         cy.get('.onscroll-fade').click()
-
         cy.get(".header-register-button").should("be.visible").should("contain.text", registerBtnTxt)
             .click()
         cy.url().should("equal", registerLink)
         cy.title().should("contain", "Free trial of Pipedrive - sign up | Pipedrive")
+    })
 
-        //header
+  it("DOM head check",
+      () => {
 
-        //logo on the left
-        cy.get(".puco-header__content").children()
-            .should("exist").should("be.visible")
-            .should('have.length', 3)
-            .eq(0).should("have.css", {
-          "height": "32px",
-          "width": "172.922px"
-        })
-
-        //artifact -- Menu from the landing page
-        cy.get(".puco-header__content").children().eq(1)
-            .should("exist")
-            .should("have.css", 'visibility', "hidden") //a-ha, an artifact
-
-        //menu on the right
-
-        //Language dropdown
-        cy.get(".puco-header__content").children().eq(2).children()
-            .should("exist")
-            .should("have.length", 2)
-            .eq(0).children().should("have.length", 2)
-            .eq(1).as('languageDropdown').should("have.css", "visibility", "hidden")
-        cy.get(".puco-header__content").children().eq(2).children().first()
-            .children().eq(0).trigger('mouseover')
-        cy.get("@languageDropdown").should('be.visible')
-            .children().first().children().as("languages")
-
-        //Checking the contents of language dropdowns
-        cy.get('@languages').each(($a, index) => {
-          cy.wrap($a).then(($a) => {
-            const expectedLink = listOfLanguagesLinks[index]
-            const expectedText = listOfLanguages[index]
-
-            const actualLink = $a.attr('href')
-            expect(actualLink).to.eq(expectedLink)
-            cy.wrap($a)
-                .children().first()
-                .invoke('text')
-                .then((text) => {
-                  expect(text).to.contain(expectedText)
-                })
+          //logo on the left
+          cy.get(".puco-header__content").children()
+              .should("exist").should("be.visible")
+              .should('have.length', 3)
+              .eq(0).should("have.css", {
+              "height": "32px",
+              "width": "172.922px"
           })
 
+          //artifact -- Menu from the landing page
+          cy.get(".puco-header__content").children().eq(1)
+              .should("exist")
+              .should("have.css", 'visibility', "hidden") //a-ha, an artifact
 
-        });
+          //menu on the right
+                //Language dropdown
+          cy.get(".puco-header__content").children().eq(2).children()
+              .should("exist")
+              .should("have.length", 2)
+              .eq(0).children().should("have.length", 2)
+              .eq(1).as('languageDropdown').should("have.css", "visibility", "hidden")
+          cy.get(".puco-header__content").children().eq(2).children().first()
+              .children().eq(0).trigger('mouseover')
+          cy.get("@languageDropdown").should('be.visible')
+              .children().first().children().as("languages")
 
-        //Log in button
-        cy.get(".puco-header__content").children().eq(2).children().eq(1)
-            .should("exist").should("be.visible")
-            .should("have.attr", "href", loginTabURL)
-            .should("contain", "Log in")
+          //Checking the contents of language dropdowns
 
-        //body
+          cy.get('@languages').each(($a, index) => {
+              cy.wrap($a).then(($a) => {
+                  const expectedLink = listOfLanguagesLinks[index]
+                  const expectedText = listOfLanguages[index]
+
+                  const actualLink = $a.attr('href')
+                  expect(actualLink).to.eq(expectedLink)
+                  cy.wrap($a)
+                      .children().first()
+                      .invoke('text')
+                      .then((text) => {
+                          expect(text).to.contain(expectedText)
+                      })
+              })
+          });
+          //menu on the right
+             //Login button
+
+          cy.get(".puco-header__content").children().eq(2).children().eq(1)
+              .should("exist").should("be.visible")
+              .should("have.attr", "href", loginTabURL)
+              .should("contain", "Log in")
+      })
+
+    it("DOM body check", () => {
+
+
         //body container dimensions and bg colour
+
         cy.get(".register_registerBlock__zt6_U .puco-container__content")
             .should("exist").should("be.visible")
             .should("have.css", {
@@ -129,6 +138,7 @@ describe('Register Page UI', () => {
             }).parent().should("have.css", "background-color", colorWhite)
 
         // h3 header in the main container
+
         cy.get(".puco-signup-modal__header").should("exist").should("be.visible")
             .should('have.css', {
               "text-align": h3TextAlign,
@@ -141,6 +151,7 @@ describe('Register Page UI', () => {
             .should("contain.text", "Try Pipedrive free for 14 days")
 
         //header bottom text
+
         cy.get(".puco-text--weight-normal.puco-text--align-horizontal-xl-center").should("exist")
             .should("be.visible")
             .should("have.css", {
@@ -154,15 +165,20 @@ describe('Register Page UI', () => {
             .should("contain.text", "Full access. No credit card needed.")
 
         //email field
+
         cy.get(".puco-input-group").children().should("exist").should("be.visible")
             .should("have.length", 3)
+
         //check for it to be empty and css values
+
         cy.get(".puco-input--prefix").should('have.attr', 'value').should("equal", "")
         cy.get(".puco-input--prefix").should("have.css", {
           "height": fieldHeight,
           "width": fieldWidth
         })
+
         //check text and its behaviour
+
         cy.get(".puco-input-label").should("exist").should("be.visible")
             .should("contain.text","Work email")
             .should("have.css",  {
@@ -172,18 +188,22 @@ describe('Register Page UI', () => {
               "position": "absolute",
             })
         cy.get('.puco-input-label').type("abcd")
+
         //check for letterbox icon
+
         cy.get(".puco-input-group").children().eq(2)
             .should('have.attr', "xmlns", "http://www.w3.org/2000/svg")
 
-
         //first checkbox
+
         cy.get(".puco-spacing--bottom-xl-s").should("exist").should("be.visible")
             .should("not.be.checked")
             .should("contain.text", 'Get helpful tips, product updates and exclusive offers via email')
+
         // I could not find a way to verify that the checkbox is indeed checked after click
         // cy.get(".puco-spacing--bottom-xl-s").click().children().first()
         // .children().eq(1).should("be.checked").click().should("not.be.checked")
+
         cy.get(".puco-spacing--bottom-xl-s").children().first().children().eq(2)
             .children().first().children().first().children().first()
             .should('have.css', {
@@ -194,6 +214,7 @@ describe('Register Page UI', () => {
               "font-family": fontFamily //and so on....
             })
         //second checkbox
+
         cy.get('.puco-spacing--bottom-xl-m').should("exist").should("be.visible")
             .should("not.be.checked")
             .should("contain.text", "By signing up, you accept our Terms of Service and Privacy Notice.")
@@ -206,14 +227,16 @@ describe('Register Page UI', () => {
           "line-height": fontLineHeight22,
           "font-family": fontFamily //and so on....
         })
+
         //check for the correct Terms of Service and Privacy Notice
+
         cy.get("@TOSandPN").children().eq(0).should("have.attr", "href",tOSandPN[0])
         cy.get("@TOSandPN").children().eq(1).should('have.attr', "href",tOSandPN[1])
 
         //continue button
+
         cy.get(".puco-button--primary").should("exist").should("be.visible")
         cy.get(".puco-button--primary").children().first().should('have.css', {
-          //"align-items": h3TextAlign,
           "font-size": "22px",
           "color": colorWhite,
           "line-height": "32px",
@@ -223,12 +246,14 @@ describe('Register Page UI', () => {
         })
 
         //under button text
+
         cy.get(".puco-signup-modal__footer").should("exist").should("be.visible")
             .children().first().as("btnFooterTxt").should("contain", "Already have an account? ")
         cy.get("@btnFooterTxt").children().first().should("have.attr", "href",loginTabURL)
             .should("contain.text", "Log in")
 
         //separator text
+
         cy.get(".puco-separator__text").should("exist").should("be.visible")
             .should("contain.text", "or access quickly")
             .should("have.css",{
@@ -240,6 +265,7 @@ describe('Register Page UI', () => {
             })
 
         //bottom buttons
+
         cy.get(".puco-button--size-m").should("exist").should("be.visible")
             .eq(0)
             .should("have.attr",  "href", "https://app.pipedrive.com/auth/google/login")
@@ -266,8 +292,12 @@ describe('Register Page UI', () => {
               "align-items" : "center"
             })
             .children().eq(1).should("contain.text", "LinkedIn")
+    })
+
+    it("DOM footer check", () => {
 
         //footer
+
         cy.get(".puco-footer").should("exist").should("be.visible")
         cy.get('.puco-footer__bottom-links').children('a').should('have.length', 4)
         cy.get('.puco-footer__text').should('have.text', 'Pipedrive is a Web-based Sales CRM.')
@@ -280,11 +310,5 @@ describe('Register Page UI', () => {
               cy.wrap($link).should('have.text', expectedLink.text)
             })
         cy.get('span.puco-text--weight-normal').eq(5).should('have.text', '© 2023 Pipedrive');
-
-
-
-        //cy.visit(loginTabURL)
-
-
-      })
+    })
 })
